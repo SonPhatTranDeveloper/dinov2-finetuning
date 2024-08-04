23df3dba5a799baae40333bae206890e9b4b63fd
Phat Tran <phat.tran.developer@gmail.com>
2024-08-04 15:59:29 +1000

    Update initial weight

--- dinov2/layers/sinkhorn.py
+++ dinov2/layers/sinkhorn.py
@@ -185,7 +185,7 @@ class WeightedCombinationAttention(nn.Module):
                  proj_drop=0.0,
                  max_iter=3,
                  eps=1,
-                 sinkhorn_weight=0.1):
+                 sinkhorn_weight=0.05):
         """
         Initialize the scaled product attention sinkhorn normalization block
         :param dim: the number of features of the input
